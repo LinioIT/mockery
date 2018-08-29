68028fc574d2bc0700b22e2a47e375e36b345870
Alex Rojas <alex.rojas@linio.com>
2018-08-29 16:32:49 -0500

    Updated mockery to work with phpunit 7.3

--- library/Mockery/Adapter/Phpunit/TestListener.php
+++ library/Mockery/Adapter/Phpunit/TestListener.php
@@ -20,56 +20,33 @@
 
 namespace Mockery\Adapter\Phpunit;
 
-use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\Test;
-use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\TestListenerDefaultImplementation;
-use PHPUnit\Runner\BaseTestRunner;
 use PHPUnit\Framework\TestListener as TestListenerBase;
+use PHPUnit\Framework\TestListenerDefaultImplementation;
+use PHPUnit\Framework\TestSuite;
 
 class TestListener implements TestListenerBase
 {
     use TestListenerDefaultImplementation;
 
+    private $trait;
+
+    public function __construct()
+    {
+        $this->trait = new TestListenerTrait();
+    }
     /**
-     * endTest is called after each test and checks if \Mockery::close() has
-     * been called, and will let the test fail if it hasn't.
-     *
-     * @param Test  $test
-     * @param float $time
+     * {@inheritdoc}
      */
     public function endTest(Test $test, float $time): void
     {
-        if (!$test instanceof TestCase) {
-            // We need the getTestResultObject and getStatus methods which are
-            // not part of the interface.
-            return;
-        }
-
-        if ($test->getStatus() !== BaseTestRunner::STATUS_PASSED) {
-            // If the test didn't pass there is no guarantee that
-            // verifyMockObjects and assertPostConditions have been called.
-            // And even if it did, the point here is to prevent false
-            // negatives, not to make failing tests fail for more reasons.
-            return;
-        }
-
-        try {
-            // The self() call is used as a sentinel. Anything that throws if
-            // the container is closed already will do.
-            \Mockery::self();
-        } catch (\LogicException $_) {
-            return;
-        }
-
-        $e = new ExpectationFailedException(
-            sprintf(
-                "Mockery's expectations have not been verified. Make sure that \Mockery::close() is called at the end of the test. Consider using %s\MockeryPHPUnitIntegration or extending %s\MockeryTestCase.",
-                __NAMESPACE__,
-                __NAMESPACE__
-            )
-        );
-        $result = $test->getTestResultObject();
-        $result->addFailure($test, $e, $time);
+        $this->trait->endTest($test, $time);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function startTestSuite(TestSuite $suite): void
+    {
+        $this->trait->startTestSuite();
     }
 }
